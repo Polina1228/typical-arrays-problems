5767cd947494ad7c4697dca1581452b4d150c39d
Polina Malashchuk <polinamalashchukk@gmail.com>
2020-10-02 04:36:54 +0300

    feat: implement min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        return Math.min(...array);
+    } else { return 0 }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        return Math.max(...array);
+    } else { return 0 }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += parseInt(array[i]);
+        }
+        const avg = sum / array.length;
+        return avg;
+    } else { return 0 }
 }
